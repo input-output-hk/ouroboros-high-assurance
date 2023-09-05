4235012cbaaee59986ff1c15d1194c11cef2881d
Wolfgang Jeltsch <wolfgang@well-typed.com>
2023-09-06 00:39:00 +0300

    Switch to arbitrary party types with absolute denotations

--- src/Ouroboros-Mini_Protocols.thy
+++ src/Ouroboros-Mini_Protocols.thy
@@ -12,20 +12,6 @@ theory "Ouroboros-Mini_Protocols"
     "HOL-Eisbach.Eisbach"
 begin
 
-subsection \<open>Parties\<close>
-
-text \<open>
-  We introduce values for denoting parties that are not labeled with the absolute terms “client” and
-  “server” but the relative terms “us” and “them”. This enables us to sometimes use the same code
-  for handling clients and servers, as it depends on the viewpoint who “us” and “them” are.
-\<close>
-
-datatype party = Us | Them
-
-primrec other_party :: "party \<Rightarrow> party" where
-  "other_party Us = Them" |
-  "other_party Them = Us"
-
 subsection \<open>Inclusion of a “Done” Case\<close>
 
 text \<open>
@@ -58,13 +44,15 @@ text \<open>
   reminiscent of Conway’s games as discussed in \<^emph>\<open>On Numbers and Games\<close>.
 \<close>
 
-codatatype 'm possibilities =
-  Possibilities (agent: \<open>party\<close>) (can_finish: \<open>bool\<close>) (next_possibilities: \<open>'m \<rightharpoonup> 'm possibilities\<close>)
+codatatype ('p, 'm) possibilities = Possibilities
+  (agent: \<open>'p\<close>)
+  (can_finish: \<open>bool\<close>)
+  (next_possibilities: \<open>'m \<rightharpoonup> ('p, 'm) possibilities\<close>)
 
 text \<open>
-  Note that besides the additional \<^const>\<open>agent\<close> field, the \<^type>\<open>possibilities\<close> type
-  corresponds exactly to the \<open>language\<close> type introduced in Subsection~2.2 of the \<^emph>\<open>corec\<close>
-  tutorial.
+  Note that besides the additional type parameter~\<^typ>\<open>'p\<close> and the additional \<^const>\<open>agent\<close>
+  field, the \<^type>\<open>possibilities\<close> type corresponds exactly to the \<open>language\<close> type introduced in
+  Subsection~2.2 of the \<^emph>\<open>corec\<close> tutorial.
 
   Possibilities always denote a situation where at least one message will still be transmitted. The
   \<^const>\<open>can_finish\<close> field being \<^const>\<open>True\<close> signals that it is possible for a “done” message
@@ -72,21 +60,11 @@ text \<open>
 \<close>
 
 text \<open>
-  By swapping the notions of “us” and “them” and thus changing the viewpoint, we obtain the dual of
-  given possibilities.
-\<close>
-
-primcorec dual_possibilities :: "'m possibilities \<Rightarrow> 'm possibilities" (\<open>_\<^sup>\<bottom>\<close> [1000] 1000) where
-  "agent P\<^sup>\<bottom> = other_party (agent P)" |
-  "can_finish P\<^sup>\<bottom> = can_finish P" |
-  "next_possibilities P\<^sup>\<bottom> = map_option dual_possibilities \<circ> next_possibilities P"
-
-text \<open>
   We introduce a relation that tells what possibilities allow what messages to be transmitted next,
   where messages include the “done” message.
 \<close>
 
-primrec permit :: "'m possibilities \<Rightarrow> 'm or_done \<Rightarrow> bool" (infix \<open>\<turnstile>\<close> 50) where
+primrec permit :: "('p, 'm) possibilities \<Rightarrow> 'm or_done \<Rightarrow> bool" (infix \<open>\<turnstile>\<close> 50) where
   "P \<turnstile> Done \<longleftrightarrow> can_finish P" |
   "P \<turnstile> Cont m \<longleftrightarrow> m \<in> dom (next_possibilities P)"
 
@@ -96,22 +74,15 @@ text \<open>
   are.
 \<close>
 
-definition follow_up :: "'m possibilities \<Rightarrow> 'm or_done \<Rightarrow> 'm possibilities or_done" where
+definition follow_up :: "('p, 'm) possibilities \<Rightarrow> 'm or_done \<Rightarrow> ('p, 'm) possibilities or_done" where
   [simp]: "follow_up P M = map_or_done (the \<circ> next_possibilities P) M" if "P \<turnstile> M"
 
 text \<open>
-  The communication patterns that a protocol permits are characterized by the possibilities of the
-  client and the server, where the latter can be derived from the former via dualization.
+  The communication patterns that a protocol permits are specified by possibilities.
 \<close>
 
 locale protocol_possibilities =
-  fixes client_possibilities :: "'m possibilities"
-begin
-
-definition server_possibilities :: "'m possibilities" where
-  [simp]: "server_possibilities = client_possibilities\<^sup>\<bottom>"
-
-end
+  fixes possibilities :: "('p, 'm) possibilities"
 
 subsection \<open>State Machines\<close>
 
@@ -120,9 +91,9 @@ text \<open>
   states.
 \<close>
 
-record ('s, 'm) state_machine =
+record ('s, 'p, 'm) state_machine =
   initial_state :: 's
-  agent_in_state :: "'s \<Rightarrow> party"
+  agent_in_state :: "'s \<Rightarrow> 'p"
   can_finish_in_state :: "'s \<Rightarrow> bool"
   next_state :: "'s \<Rightarrow> 'm \<rightharpoonup> 's"
 
@@ -133,102 +104,44 @@ text \<open>
 \<close>
 
 text \<open>
-  By swapping the notions of “us” and “them” and thus changing the viewpoint, we obtain the dual of
-  a given state machine.
-\<close>
-
-definition
-  dual_state_machine :: "('s, 'm) state_machine \<Rightarrow> ('s, 'm) state_machine"
-  ("_\<^sup>\<bottom>" [1000] 1000)
-where
-  [simp]: "S\<^sup>\<bottom> = S \<lparr>agent_in_state := other_party \<circ> agent_in_state S\<rparr>"
-
-text \<open>
   We define as the meaning of a state machine the possibilities corresponding to it. As a result, we
   obtain a state machine semantics whose essence is forgetting about states.
 \<close>
 
-primcorec state_machine_semantics :: "('s, 'm) state_machine \<Rightarrow> 'm possibilities" (\<open>\<lbrakk>_\<rbrakk>\<close>) where
+primcorec state_machine_semantics :: "('s, 'p, 'm) state_machine \<Rightarrow> ('p, 'm) possibilities" (\<open>\<lbrakk>_\<rbrakk>\<close>) where
   "agent \<lbrakk>S\<rbrakk> = agent_in_state S (initial_state S)" |
   "can_finish \<lbrakk>S\<rbrakk> = can_finish_in_state S (initial_state S)" |
   "next_possibilities \<lbrakk>S\<rbrakk> = map_option (\<lambda>s. \<lbrakk>S \<lparr>initial_state := s\<rparr>\<rbrakk>) \<circ> next_state S (initial_state S)"
 
 text \<open>
-  The state machine semantics preserves dualization.
-\<close>
-
-lemma state_machine_semantics_preserves_dualization:
-  fixes S :: "('s, 'm) state_machine"
-  shows "\<lbrakk>S\<^sup>\<bottom>\<rbrakk> = \<lbrakk>S\<rbrakk>\<^sup>\<bottom>"
-proof (coinduction arbitrary: S)
-  case Eq_possibilities
-  have "agent \<lbrakk>S\<^sup>\<bottom>\<rbrakk> = agent \<lbrakk>S\<rbrakk>\<^sup>\<bottom>"
-    by simp
-  moreover
-  have "can_finish \<lbrakk>S\<^sup>\<bottom>\<rbrakk> = can_finish \<lbrakk>S\<rbrakk>\<^sup>\<bottom>"
-    by simp
-  moreover
-  have "
-    rel_option (\<lambda>P\<^sub>1 P\<^sub>2. \<exists>S' :: ('s, 'm) state_machine. P\<^sub>1 = \<lbrakk>S'\<^sup>\<bottom>\<rbrakk> \<and> P\<^sub>2 = \<lbrakk>S'\<rbrakk>\<^sup>\<bottom>)
-      (next_possibilities \<lbrakk>S\<^sup>\<bottom>\<rbrakk> m)
-      (next_possibilities \<lbrakk>S\<rbrakk>\<^sup>\<bottom> m)"
-    for m
-  proof (cases \<open>next_state S (initial_state S) m\<close>)
-    case None
-    then show ?thesis
-      by simp
-  next
-    case (Some s)
-    define S' where "S' = S \<lparr>initial_state := s\<rparr>"
-    with Some have "next_possibilities \<lbrakk>S\<^sup>\<bottom>\<rbrakk> m = Some \<lbrakk>S'\<^sup>\<bottom>\<rbrakk>" and "next_possibilities \<lbrakk>S\<rbrakk>\<^sup>\<bottom> m = Some \<lbrakk>S'\<rbrakk>\<^sup>\<bottom>"
-      by (cases \<open>S\<close>, simp_all)
-    then show ?thesis
-      by (auto simp only: option.simps)
-  qed
-  ultimately show ?case
-    by blast
-qed
-
-text \<open>
   The possibilities that characterize the communication patterns that a protocol permits can be
-  described using state machines. A state machine for the client is sufficient for specifying the
-  permitted communication patterns, since the state machine for the server can be derived from it
-  via dualization.
+  described using a state machine.
 \<close>
 
-locale protocol_state_machines =
-  fixes client_state_machine :: "('s, 'm) state_machine"
+locale protocol_state_machine =
+  fixes state_machine :: "('s, 'p, 'm) state_machine"
 begin
 
-definition server_state_machine :: "('s, 'm) state_machine" where
-  [simp]: "server_state_machine = client_state_machine\<^sup>\<bottom>"
+definition possibilities :: "('p, 'm) possibilities" where
+  [simp]: "possibilities = \<lbrakk>state_machine\<rbrakk>"
 
-definition client_possibilities :: "'m possibilities" where
-  [simp]: "client_possibilities = \<lbrakk>client_state_machine\<rbrakk>"
-
-sublocale protocol_possibilities \<open>client_possibilities\<close> .
-
-lemma server_possibilities_from_state_machine:
-  shows "server_possibilities = \<lbrakk>server_state_machine\<rbrakk>"
-  unfolding server_possibilities_def and server_state_machine_def
-  unfolding client_possibilities_def
-  using state_machine_semantics_preserves_dualization [symmetric] .
+sublocale protocol_possibilities \<open>possibilities\<close> .
 
 end
 
 subsection \<open>Programs\<close>
 
 text \<open>
-  A program implements either the client or the server part of a protocol.
+  A program describes the part of the execution of a protocol that is performed by a single party.
 \<close>
 
-codatatype ('M, 'r) program =
-  Return \<open>'r\<close> (\<open>\<triangle> _\<close> [56] 55) |
-  Send \<open>'M\<close> \<open>('M, 'r) program\<close> (\<open>\<up> _;/ _\<close> [0, 55] 55) |
-  Receive \<open>'M \<rightharpoonup> ('M, 'r) program\<close>
+codatatype 'M program =
+  Finish (\<open>\<bottom>\<close>) |
+  Send \<open>'M\<close> \<open>'M program\<close> (\<open>\<up> _;/ _\<close> [0, 55] 55) |
+  Receive \<open>'M \<rightharpoonup> 'M program\<close>
 
 syntax
-  "_Receive" :: "pttrn \<Rightarrow> ('M, 'r) program \<Rightarrow> ('M, 'r) program"
+  "_Receive" :: "pttrn \<Rightarrow> 'M program \<Rightarrow> 'M program"
   (\<open>\<down> _;/ _\<close> [0, 55] 55)
 translations
   "\<down> M; \<Pi>" \<rightleftharpoons> "CONST Receive (\<lambda>M. \<Pi>)"
@@ -242,10 +155,10 @@ text \<open>
   intend the corresponding type parameter to be instantiated with message types that include “done”
   messages, that is, types of the form \<^typ>\<open>'m or_done\<close>. It might have been better to enforce the
   possibility of sending and receiving “done” messages by defining \<^type>\<open>program\<close> as follows:\<^theory_text>\<open>
-    codatatype ('m, 'r) program =
-      Return \<open>'r\<close> (\<open>\<triangle> _\<close> [56] 55) |
-      Send \<open>'m or_done\<close> \<open>('m, 'r) program\<close> (\<open>\<up> _;/ _\<close> [0, 55] 55) |
-      Receive \<open>'m or_done \<rightharpoonup> ('m, 'r) program\<close>
+    codatatype 'm program =
+      Finish (\<open>\<bottom>\<close>) |
+      Send \<open>'m or_done\<close> \<open>'m program\<close> (\<open>\<up> _;/ _\<close> [0, 55] 55) |
+      Receive \<open>'m or_done \<rightharpoonup> 'm program\<close>
   \<close>
   However, the construction of concrete programs will typically use the \<^emph>\<open>corec\<close> package, which in
   Isabelle2022 cannot cope with this definition. The relevant issues are being discussed in the
@@ -257,37 +170,39 @@ text \<open>
 \<close>
 
 text \<open>
-  We introduce a relation that tells what programs conform to what communication schemes, where a
-  communication scheme is either specified by possibilities or is a special “done” scheme, which
-  does not permit any further communication.
+  We introduce a relation that tells what it means for a program to conform to a certain
+  communication scheme when run by a certain party, where a communication scheme is either specified
+  by possibilities or is a special “done” scheme, which does not permit any further communication.
 \<close>
 
 coinductive
-  conforms_to :: "('m or_done, 'r) program \<Rightarrow> 'm possibilities or_done \<Rightarrow> bool"
-  (infix \<open>\<Colon>\<close> 50)
+  conformance :: "'p \<Rightarrow> 'm or_done program \<Rightarrow> ('p, 'm) possibilities or_done \<Rightarrow> bool"
+  (\<open>'(\<Colon>\<^bsub>_\<^esub>')\<close>)
+and
+  conformance_std :: "'m or_done program \<Rightarrow> 'p \<Rightarrow> ('p, 'm) possibilities or_done \<Rightarrow> bool"
+  (\<open>(_ \<Colon>\<^bsub>_\<^esub>/ _)\<close> [51, 0, 51] 50)
+for
+  p :: 'p
 where
-  return_conformance:
-    "\<triangle> _ \<Colon> Done" |
+  "\<Pi> \<Colon>\<^bsub>p\<^esub> \<P> \<equiv> (\<Colon>\<^bsub>p\<^esub>) \<Pi> \<P>" |
+  finish_conformance:
+    "\<bottom> \<Colon>\<^bsub>p\<^esub> Done" |
   send_conformance:
-    "\<up> M; \<Pi> \<Colon> Cont P"
-      if "agent P = Us" and "P \<turnstile> M" and "\<Pi> \<Colon> follow_up P M" |
+    "\<up> M; \<Pi> \<Colon>\<^bsub>p\<^esub> Cont P"
+      if "agent P = p" and "P \<turnstile> M" and "\<Pi> \<Colon>\<^bsub>p\<^esub> follow_up P M" |
   receive_conformance:
-    "\<down> M; \<Xi> M \<Colon> Cont P"
-      if "agent P = Them" and "dom \<Xi> = {M. P \<turnstile> M}" and "\<forall>M \<in> dom \<Xi>. the (\<Xi> M) \<Colon> follow_up P M"
+    "\<down> M; \<Xi> M \<Colon>\<^bsub>p\<^esub> Cont P"
+      if "agent P \<noteq> p" and "dom \<Xi> = {M. P \<turnstile> M}" and "\<forall>M \<in> dom \<Xi>. the (\<Xi> M) \<Colon>\<^bsub>p\<^esub> follow_up P M"
 
 text \<open>
-  A protocol implementation consists of a client and a server program that conform to the
-  possibilities of the protocol.
+  A protocol is implemented by assigning programs to parties such that the programs communicate
+  according to the possibilities of the protocol.
 \<close>
 
-locale protocol_programs =
-  protocol_possibilities \<open>client_possibilities\<close> for client_possibilities :: "'m possibilities" +
-  fixes client_program :: "('m or_done, 'r\<^sub>c) program"
-  fixes server_program :: "('m or_done, 'r\<^sub>s) program"
-  assumes client_conformance:
-    "client_program \<Colon> Cont client_possibilities"
-  assumes server_conformance:
-    "server_program \<Colon> Cont server_possibilities"
+locale protocol_programs = protocol_possibilities +
+  fixes program :: "'p \<Rightarrow> 'm or_done program"
+  assumes conformance:
+    "\<And>p. program p \<Colon>\<^bsub>p\<^esub> Cont possibilities"
 
 subsection \<open>Coinduction Up to Embedding\<close>
 
@@ -307,45 +222,50 @@ text \<open>
 subsubsection \<open>The Proof Principle\<close>
 
 text \<open>
-  We introduce an endofunction on relations that captures the “up to embedding” notion and a variant
-  of it that excludes the possibility of having just holes with nothing around them.
+  We introduce a party-indexed family of endofunctions on relations that captures the “up to
+  embedding” notion and a variant of it that excludes the possibility of having just holes with
+  nothing around them.
 \<close>
 
 coinductive
   up_to_embedding :: "
-    (('m or_done, 'r) program \<Rightarrow> 'm possibilities or_done \<Rightarrow> bool) \<Rightarrow>
-    (('m or_done, 'r) program \<Rightarrow> 'm possibilities or_done \<Rightarrow> bool)"
+    'p \<Rightarrow>
+    ('m or_done program \<Rightarrow> ('p, 'm) possibilities or_done \<Rightarrow> bool) \<Rightarrow>
+    ('m or_done program \<Rightarrow> ('p, 'm) possibilities or_done \<Rightarrow> bool)"
 and
   up_to_actual_embedding :: "
-    (('m or_done, 'r) program \<Rightarrow> 'm possibilities or_done \<Rightarrow> bool) \<Rightarrow>
-    (('m or_done, 'r) program \<Rightarrow> 'm possibilities or_done \<Rightarrow> bool)"
+    'p \<Rightarrow>
+    ('m or_done program \<Rightarrow> ('p, 'm) possibilities or_done \<Rightarrow> bool) \<Rightarrow>
+    ('m or_done program \<Rightarrow> ('p, 'm) possibilities or_done \<Rightarrow> bool)"
 for
-  R :: "('m or_done, 'r) program \<Rightarrow> 'm possibilities or_done \<Rightarrow> bool"
+  p :: 'p
+and
+  R :: "'m or_done program \<Rightarrow> ('p, 'm) possibilities or_done \<Rightarrow> bool"
 where
   up_to_no_actual_embedding:
-    "up_to_embedding R \<Pi> \<P>"
+    "up_to_embedding p R \<Pi> \<P>"
       if "R \<Pi> \<P>" |
   up_to_actual_embedding:
-    "up_to_embedding R \<Pi> \<P>"
-      if "up_to_actual_embedding R \<Pi> \<P>" |
-  up_to_done_embedding:
-    "up_to_actual_embedding R (\<triangle> _) Done" |
+    "up_to_embedding p R \<Pi> \<P>"
+      if "up_to_actual_embedding p R \<Pi> \<P>" |
+  up_to_finish_embedding:
+    "up_to_actual_embedding p R \<bottom> Done" |
   up_to_send_embedding:
-    "up_to_actual_embedding R (\<up> M; \<Pi>) (Cont P)"
+    "up_to_actual_embedding p R (\<up> M; \<Pi>) (Cont P)"
       if
-        "agent P = Us"
+        "agent P = p"
       and
         "P \<turnstile> M"
       and
-        "up_to_embedding R \<Pi> (follow_up P M)" |
+        "up_to_embedding p R \<Pi> (follow_up P M)" |
   up_to_receive_embedding:
-    "up_to_actual_embedding R (\<down> M; \<Xi> M) (Cont P)"
+    "up_to_actual_embedding p R (\<down> M; \<Xi> M) (Cont P)"
       if
-        "agent P = Them"
+        "agent P \<noteq> p"
       and
         "dom \<Xi> = {M. P \<turnstile> M}"
       and
-        "\<forall>M \<in> dom \<Xi>. up_to_embedding R (the (\<Xi> M)) (follow_up P M)"
+        "\<forall>M \<in> dom \<Xi>. up_to_embedding p R (the (\<Xi> M)) (follow_up P M)"
 
 text \<open>
   Coinduction up to embedding is sound. The following lemma not only states this soundness but also
@@ -353,15 +273,15 @@ text \<open>
 \<close>
 
 lemma up_to_embedding_is_sound [case_names bisimulation]:
-  assumes "R \<Pi> \<P>" and "\<And>\<Pi> \<P>. R \<Pi> \<P> \<Longrightarrow> up_to_actual_embedding R \<Pi> \<P>"
-  shows "\<Pi> \<Colon> \<P>"
+  assumes "R \<Pi> \<P>" and "\<And>\<Pi> \<P>. R \<Pi> \<P> \<Longrightarrow> up_to_actual_embedding p R \<Pi> \<P>"
+  shows "\<Pi> \<Colon>\<^bsub>p\<^esub> \<P>"
 proof -
-  from \<open>R \<Pi> \<P>\<close> have \<open>up_to_embedding R \<Pi> \<P>\<close>
+  from \<open>R \<Pi> \<P>\<close> have \<open>up_to_embedding p R \<Pi> \<P>\<close>
     by (rule up_to_no_actual_embedding)
   then show ?thesis
   proof (coinduction arbitrary: \<Pi> \<P>)
-    case conforms_to
-    from \<open>up_to_embedding R \<Pi> \<P>\<close> have "up_to_actual_embedding R \<Pi> \<P>"
+    case conformance
+    from \<open>up_to_embedding p R \<Pi> \<P>\<close> have "up_to_actual_embedding p R \<Pi> \<P>"
     proof cases
       case up_to_no_actual_embedding
       with assms(2) show ?thesis .
@@ -380,7 +300,7 @@ text \<open>
 
     \<^item> Both terms make corresponding steps.
 
-    \<^item> The target terms of these steps are related by \<^term>\<open>up_to_embedding R\<close>.
+    \<^item> The target terms of these steps are related by \<^term>\<open>up_to_embedding p R\<close>.
 \<close>
 
 subsubsection \<open>Automatic Construction of Bisimulation Proofs\<close>
@@ -406,21 +326,16 @@ text \<open>
   simplification rules for such constants must be made part of the simpset in order for the prover
   to cope with these constants.
 
-  The \<^theory_text>\<open>state_machine_bisimulation\<close> method also takes the definitions of
-  \<^const>\<open>protocol_state_machines.client_possibilities\<close> and
-  \<^const>\<open>protocol_possibilities.server_possibilities\<close> into account, which frees the user from
-  manually initiating the unfolding of these constants when interpreting
-  \<^locale>\<open>protocol_programs\<close> in the presence of a \<^locale>\<open>protocol_state_machines\<close>
-  interpretation.
+  The \<^theory_text>\<open>state_machine_bisimulation\<close> method also takes the definition of
+  \<^const>\<open>protocol_state_machine.possibilities\<close> into account, which frees the user from manually
+  initiating the unfolding of this constant when interpreting \<^locale>\<open>protocol_programs\<close> in the
+  presence of a \<^locale>\<open>protocol_state_machine\<close> interpretation.
 \<close>
 
 method state_machine_bisimulation uses program_expansion extra_splits = (
   subst (2) program_expansion,
   fastforce
-    simp add:
-      protocol_state_machines.client_possibilities_def
-      protocol_possibilities.server_possibilities_def
-      domIff
+    simp add: protocol_state_machine.possibilities_def domIff
     split: extra_splits
     intro: up_to_embedding_up_to_actual_embedding.intros
 )
