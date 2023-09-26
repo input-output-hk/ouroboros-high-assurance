8df2f814c6a34b6ebc820d37df0a3df5da062002
Javier Díaz <javier.diaz@iohk.io>
2023-09-26 18:22:49 -0300

    Add names to locale assumptions

--- src/Ouroboros-Mini_Protocols-Chain_Sync.thy
+++ src/Ouroboros-Mini_Protocols-Chain_Sync.thy
@@ -21,8 +21,10 @@ locale chain_sync =
   fixes candidate_points :: "'i list \<Rightarrow> 'q list"
   fixes initial_client_chain :: "'i list"
   fixes initial_server_chain :: "'i list"
-  assumes "initial_client_chain \<noteq> []"
-  assumes "initial_server_chain \<noteq> []"
+  assumes genesis_item_in_initial_client_chain:
+    "initial_client_chain \<noteq> []"
+  assumes genesis_item_in_initial_server_chain:
+    "initial_server_chain \<noteq> []"
 
 subsection \<open>Parties\<close>
 
